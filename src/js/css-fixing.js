3bc4eb666bf985b03fe232db22a50e7a5fab2e4d
Steven Doolan <doolansr@rose-hulman.edu>
2016-02-03 11:49:13 -0500

    css updated, removed top property, condensed to a singular layer 1, inserted jquery height

---
+++ src/js/css-fixing.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Steven on 2/3/2016.
+ */
+
+//window.onload = function(){
+//  updateHeights();
+//};
+//
+//var updateHeights = function() {
+//    $('#about').height($('#about .layer2').height());
+//};
+//
+//
